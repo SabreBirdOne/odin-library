0aa811212df468f45a0b65d3ea347ed355dbad7a
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-11-11 16:31:59 -0500

    Add id property to Book object
    Generate id without arguments in Book constructor
    Modify info() method to show id

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ function Book(title, author, numPages) {
     if (!new.target) {
         throw Error("missing new before constructor call");
     }
+    
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.numPages = numPages;
     this.isRead = false;
+
     this.info = function (){
         const isReadStr = this.isRead ? "read" : "not read yet"; 
-        return `${this.title} by ${this.author}, ${this.numPages} pages, ` + isReadStr;
+        return `ID ${this.id}: ${this.title} by ${this.author}, ${this.numPages} pages, ` + isReadStr;
     }
 }
