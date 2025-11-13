e60e1823337e95f2073868ec20afdacc9442b188
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-11-13 15:01:20 -0500

    Add functions to display books in the DOM
    createBookCard creates a div for a book card
    addBookCardsToElement adds book cards to some
    DOM element from an array of Books.
    
    Add some test code after functions in main.js
    
    Add menu and books divs into the HTML

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const myLibrary = [];
-
 function Book(title, author, numPages, read) {
     if (!new.target) {
         throw Error("missing new before constructor call");
@@ -22,3 +20,34 @@ function addBookToLibrary(library, title, author, numPages, read) {
   const newBook = new Book(title, author, numPages, read);
   library.push(newBook);
 }
+
+function createBookCard (book){
+    // Book cards are divs in the HTML for each book in the library.
+    const bookCard = document.createElement("div");
+    bookCard.classList = "book";
+    bookCard.id = book.id;
+    bookCard.textContent = book.info();
+    return bookCard;
+}
+
+function addBookCardsToElement(library, booksParentElement){
+    /* 
+    PARAMETERS: 
+        library: array of Book objects
+        booksParentElement: the object to append book cards from the library
+    EFFECTS: for each Book in the library, create a Book Card and add it to the booksParentElement.
+    */
+    for (let i = 0; i < library.length; i++){ 
+        const newBookCard = createBookCard(library[i])
+        booksParentElement.appendChild(newBookCard);
+    }
+}
+
+const myLibrary = [];
+addBookToLibrary(myLibrary, "Design Patterns", "Gang of Four", 99, false);
+addBookToLibrary(myLibrary, "FOCS", "Malik", 99, true);
+addBookToLibrary(myLibrary, "How to live with CSS", "Underpaid intern", 1, false);
+
+const booksDiv = document.querySelector("div.books");
+addBookCardsToElement(myLibrary, booksDiv);
+
